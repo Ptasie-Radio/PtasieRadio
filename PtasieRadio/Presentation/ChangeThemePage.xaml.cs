296b2664305132076152e639f6c927f6988b6d82
DimmeSs <karol2016pl@gmail.com>
2025-06-07 16:20:56 +0200

    Bugfix "Scroll Jump"
    No scroll jump on theme checkbox click

--- PtasieRadio/Presentation/ChangeThemePage.xaml.cs
+++ PtasieRadio/Presentation/ChangeThemePage.xaml.cs
@@ -9,12 +9,37 @@ public sealed partial class ChangeThemePage : Page
   {
     this.InitializeComponent();
   }
-
-private void CheckBox_Click(object sender, RoutedEventArgs e)
-{
+  private void SaveScrollPosition()
+  {
+      _savedScrollOffset = MainScrollViewer.VerticalOffset;
+      ApplicationData.Current.LocalSettings.Values["SavedScroll"] = _savedScrollOffset;
+  }
+  // save scroll position żeby na małych ekranach nie mogło przewijać do góry:podczas zmiany motywu
+  private void CheckBox_Click(object sender, RoutedEventArgs e)
+  {
+    SaveScrollPosition();
     var cb = sender as CheckBox;
     // jeśli kliknięto zaznaczony już motyw – NIE pozwól go odznaczyć:
     if (cb.IsChecked == false)
-        cb.IsChecked = true;
-}     
+      cb.IsChecked = true;
+  }
+
+private double _savedScrollOffset = 0;
+
+
+
+protected override void OnNavigatedTo(NavigationEventArgs e)
+{
+    base.OnNavigatedTo(e);
+
+    MainScrollViewer.Loaded += (s, args) =>
+    {
+        if (ApplicationData.Current.LocalSettings.Values.TryGetValue("SavedScroll", out var saved) && saved is double offset)
+        {
+            MainScrollViewer.ChangeView(null, offset, null);
+        }
+    };
+}
+
+
 }
